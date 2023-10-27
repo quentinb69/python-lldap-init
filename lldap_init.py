eb9d07fc1af9cd6089d9d836fc7fd2c765f95554
Quentin B <3447731+quentinb69@users.noreply.github.com>
2023-10-27 12:13:09 +0200

    feat: refacto python

--- lldap_init.py
+++ lldap_init.py
@@ -1,88 +1,92 @@
-# From https://github.com/lldap/lldap/issues/654#issuecomment-1694251863
+# Inspired from https://github.com/lldap/lldap/issues/654#issuecomment-1694251863
 import subprocess
 import json
 import os
 
-from requests import RequestException, Session
+from requests import JSONDecodeError, RequestException, Session
 from qlient.http import HTTPBackend, HTTPClient, Fields
-
-ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD', "/config/admin_password")
-CONFIG_FILE = os.environ.get('CONFIG_FILE', "/config/config_file.json")
-
-# load configuration
-assert os.path.isfile(CONFIG_FILE) is True, "Configuration " + \
-    CONFIG_FILE + " does not exist..."
-with open(CONFIG_FILE) as config_file:
-    CONFIG = json.load(config_file)
-
-assert os.path.isfile(ADMIN_PASSWORD) is True, "Admin password file " + \
-    ADMIN_PASSWORD + " does not exist..."
-admin_password_file = ADMIN_PASSWORD
-
-assert len(CONFIG["admin_username"]) > 0, "admin_username not set..."
-admin_username = CONFIG["admin_username"]
-
-assert len(CONFIG["ldap_url"]) > 0, "ldap_url not set..."
-ldap_url = CONFIG["ldap_url"]
-
-assert len(CONFIG["web_url"]) > 0, "web_url not set..."
-web_url = CONFIG["web_url"]
-
-assert len(CONFIG["base_dn"]) > 0, "base_dn not set..."
-base_dn = CONFIG["base_dn"]
-
-assert len(CONFIG["seed"]) > 0, "seed not set..."
-groups = CONFIG["seed"].get("groups", [])
-users = CONFIG["seed"].get("users", [])
-
-# prepare session
-session = Session()
-with open(admin_password_file, "r") as f:
-    admin_password = f.read()
-
-try:
-    jwt_token = session.post(
-        url=f'{web_url}/auth/simple/login',
-        json={
-            "username": admin_username,
-            "password": admin_password,
-        }
-    ).json()["token"]
-except RequestException as e:  # This is the correct syntax
-    raise SystemExit(e)
-
-admin_password = ""
-session.headers["Authorization"] = f"Bearer {jwt_token}"
-client = HTTPClient(HTTPBackend(f'{web_url}/api/graphql', session=session))
 
 # set structures for graphQL
 GROUP_FIELDS = Fields("id", "displayName")
 USER_FIELDS = Fields("id", "email", "displayName", groups=GROUP_FIELDS)
-
-# get groups and users infos
-existing_groups = client.query.groups(GROUP_FIELDS).data["groups"]
-existing_groups_map = {group["displayName"]: group for group in existing_groups}
-existing_users = client.query.users(USER_FIELDS).data["users"]
-existing_users_map = {user["id"]: user for user in existing_users}
-
-### FUNCTIONS START ###
+# default configuration
+ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD', "/config/admin_password")
+CONFIG_FILE = os.environ.get('CONFIG_FILE', "/config/config_file.json")
 
 
-def create_user(user_original):
-    user = user_original.copy()
-    del user_original
+def validate(expression: bool, message: str = "") -> None:
+    """ Validate, to be used like Assert """
+
+    if not expression:
+        print(message)
+        exit(1)
+
+    return
+
+
+def validateConfiguration(configuration_file: str, admin_password_file: str) -> json:
+    """ Validate and return configuration dict """
+
+    validate(os.path.isfile(admin_password_file) is True, "admin_password_file '" +
+             admin_password_file + "' does not exist...")
+    validate(os.path.isfile(configuration_file) is True, "config_file '" +
+             configuration_file + "' does not exist...")
+
+    with open(configuration_file) as config_file:
+        configuration: json = json.load(config_file)
+    validate(len(configuration["admin_username"])
+             > 0, "admin_username not set...")
+    validate(len(configuration["ldap_url"]) > 0, "ldap_url not set...")
+    validate(len(configuration["web_url"]) > 0, "web_url not set...")
+    validate(len(configuration["base_dn"]) > 0, "base_dn not set...")
+    validate(len(configuration["seed"]) > 0, "seed not set...")
+
+    return configuration
+
+
+def createauthenticatedWebClient(admin_username: str, web_url: str, admin_password_file: str) -> HTTPClient:
+    """ Log into LLDAP, and return the HTTPClient to be used for next requests """
+
+    session = Session()
+    with open(admin_password_file, "r") as f:
+        admin_password = f.read()
+    try:
+        jwt_token_request = session.post(
+            url=f'{web_url}/auth/simple/login',
+            json={
+                "username": admin_username,
+                "password": admin_password,
+            }
+        )
+        admin_password = ""
+    except RequestException as e:
+        raise SystemExit(e)
+
+    validate(jwt_token_request.status_code == 200, jwt_token_request.content)
+    jwt_token = jwt_token_request.json()["token"]
+
+    session.headers["Authorization"] = f"Bearer {jwt_token}"
+    httpclient = HTTPClient(HTTPBackend(
+        f'{web_url}/api/graphql', session=session))
+
+    return httpclient
+
+
+def create_single_user(user: json, ldap_url: str, base_dn: str, admin_username: str, admin_password_file: str, client: HTTPClient) -> json:
+    """ Create a user via graphql and set its password via ldappasswd """
+
     groups = user.pop("groups", [])
     password_file = user.pop("password_file")
 
     # create user
     req = client.mutation.createUser(user=user, _fields=USER_FIELDS)
-    assert req.errors is None, req.raw
+    validate(req.errors is None, req.raw)
 
     # add to groups
     for group in groups:
         req = client.mutation.addUserToGroup(
             userId=user["id"], groupId=group["id"])
-        assert req.errors is None, req.raw
+        validate(req.errors is None, req.raw)
 
     # set password
     ret = subprocess.run([
@@ -94,34 +98,72 @@ def create_user(user_original):
         "-T", password_file,
         "uid=" + user["id"] + ",ou=people," + base_dn
     ], capture_output=True)
-    assert ret.returncode == 0, "error setting password for " + user["id"]
-    print("\tCreated user '{}'".format(user["id"]))
-### FUNCTIONS END ###
+    validate(ret.returncode == 0, "Error setting password for '" +
+             user["id"] + "'.\n" + ret.stderr.decode("utf-8"))
+    print("\tUser '{}' created.".format(user["id"]))
+
+    return user
 
 
-# create groups
-for must_exist_group in sorted(groups):
-    if must_exist_group not in existing_groups_map:
-        print(f"Group '{must_exist_group}' does not exist, creating...")
-        group = client.mutation.createGroup(
-            name=must_exist_group, _fields=GROUP_FIELDS).data["createGroup"]
-        existing_groups_map[must_exist_group] = group
-        print(f"\tCreated group '{must_exist_group}'.")
-    else:
-        print(f"Group '{must_exist_group}' exists, skipping")
+def create_all_groups(groups: json, client: HTTPClient) -> dict:
+    """ Create all groups with graphql """
 
-# create users
-for must_exist_user in sorted(users, key=lambda x: x["id"]):
-    must_exist_user_id = must_exist_user["id"]
+    existing_groups: json = client.query.groups(GROUP_FIELDS).data["groups"]
+    existing_groups_map: dict = {
+        group["displayName"]: group for group in existing_groups}
 
-    # Seed file groups is just a list of names, API returns id + displayName
-    must_exist_user["groups"][:] = [
-        existing_groups_map[group_name] for group_name in must_exist_user["groups"]
-    ]
+    for must_exist_group in sorted(groups):
+        if must_exist_group not in existing_groups_map:
+            print(f"Group '{must_exist_group}' does not exist, creating...")
+            group: json = client.mutation.createGroup(
+                name=must_exist_group, _fields=GROUP_FIELDS).data["createGroup"]
+            existing_groups_map[must_exist_group] = group
+            print(f"\tGroup '{must_exist_group}' created.")
+        else:
+            print(f"Group '{must_exist_group}' exists, skipping")
 
-    if must_exist_user_id not in existing_users_map:
-        print(f"User '{must_exist_user_id}' does not exist, creating...")
-        create_user(must_exist_user)
+    return existing_groups_map
 
-    else:
-        print(f"User '{must_exist_user_id}' exists, skipping")
+
+def create_all_users(users: json, existing_groups_map: dict, ldap_url: str, base_dn: str, admin_username: str, admin_password_file: str, client: HTTPClient) -> dict:
+    """ Create all users with graphql and ldappasswd"""
+
+    existing_users: json = client.query.users(USER_FIELDS).data["users"]
+    existing_users_map: dict = {user["id"]: user for user in existing_users}
+
+    for must_exist_user in sorted(users, key=lambda x: x["id"]):
+        must_exist_user_id: str = must_exist_user["id"]
+
+        # Seed file groups is just a list of names, API returns id + displayName
+        must_exist_user["groups"][:] = [
+            existing_groups_map[group_name] for group_name in must_exist_user["groups"]
+        ]
+
+        if must_exist_user_id not in existing_users_map:
+            print(f"User '{must_exist_user_id}' does not exist, creating...")
+            create_single_user(must_exist_user, ldap_url, base_dn,
+                               admin_username, admin_password_file, client)
+
+        else:
+            print(f"User '{must_exist_user_id}' exists, skipping")
+
+    return must_exist_user_id
+
+
+def main() -> int:
+    """Echo the input arguments to standard output"""
+
+    configuration = validateConfiguration(CONFIG_FILE, ADMIN_PASSWORD)
+    client = createauthenticatedWebClient(
+        configuration["admin_username"], configuration["web_url"], ADMIN_PASSWORD)
+    configuration_groups = configuration["seed"].get("groups", [])
+    configuration_users = configuration["seed"].get("users", [])
+    groups = create_all_groups(configuration_groups, client)
+    users = create_all_users(configuration_users, groups,
+                             configuration["ldap_url"], configuration["base_dn"], configuration["admin_username"], ADMIN_PASSWORD, client)
+    print("Finished: " + str(len(users)) + " users and " + str(len(groups)) + " groups.")
+    return 0
+
+
+if __name__ == '__main__':
+    exit(main())
